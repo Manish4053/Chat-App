6a8d6bc4bdf40f2bdd37338676638bf166c9a9b3
Manish4053 <kumarmanishsah445@gmail.com>
2025-12-28 00:21:49 +0530

    some adjustment4

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);  
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = conn.connections[0].readyState;
+    console.log("MongoDB connected:", conn.connection.host);
   } catch (error) {
-    console.log("MongoDB connection error: ", error);
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
