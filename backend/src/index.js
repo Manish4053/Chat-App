efc1862b562fcd49fa2ce92234f9bf91bc570c0e
Manish4053 <kumarmanishsah445@gmail.com>
2025-12-28 00:17:41 +0530

    some adjustment3

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
+connectDB();
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
@@ -25,5 +25,5 @@ app.use("/api/messages", messageRoutes);
 
 server.listen(PORT, () => {
   console.log("server is rinning on PORT: " + PORT);
-  connectDB();
+  
 });
